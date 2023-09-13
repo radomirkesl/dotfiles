e1d7749d7310588dfbd931d6b2e7b2d50cea05e8
kesl.radomir <kesl.radomir@tuta.io>
2023-09-13 17:47:45 +0200

    changed treesitter config in hopes of removing stupid logs on update

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -13,11 +13,11 @@ require 'nvim-treesitter.configs'.setup {
     ensure_installed = "all",
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+    sync_install = true,
 
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
+    auto_install = false,
 
     -- List of parsers to ignore installing (for "all")
     -- ignore_install = { "javascript" },
